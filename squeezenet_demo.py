979f0b68a3c09779542a3ac7d5301c145e46e1e6
Tammy Yang <tammy@dt42.io>
2016-06-16 18:53:17 +0800

    add predict action

--- squeezenet_demo.py
+++ squeezenet_demo.py
@@ -1,5 +1,11 @@
 """
-Usage: python squeezenet_demo.py -p "/home/db/www/database/tests"
+Usage:
+1. training
+python squeezenet_demo.py --action='train'\
+    -p /home/db/train -v /home/db/validation
+2. prediction
+python squeezenet_demo.py --action='predice'\
+    -p /db/Roasted-Broccoli-Pasta-Recipe-5-683x1024.jpg
 """
 import time
 import argparse
@@ -10,7 +16,7 @@ from keras.optimizers import Adam
 from keras.optimizers import SGD
 
 dp = dp_tools.DP()
-tl = tools.TOOLS()
+tl = tools.DATA()
 
 
 def main():
@@ -20,6 +26,10 @@ def main():
     parser.add_argument(
         "--batch-size", type=int, default=32, dest='batchsize',
         help="Size of the mini batch. Default: 32."
+        )
+    parser.add_argument(
+        "--action", type=str, default='train',
+        help="Action to be performed, train/predict"
         )
     parser.add_argument(
         "--epochs", type=int, default=20,
@@ -38,7 +48,7 @@ def main():
         help="Path where the images are. Default: $PWD."
         )
     parser.add_argument(
-        "-v", "--val-path", type=str, default='.', required=True,
+        "-v", "--val-path", type=str, default='.',
         dest='valpath', help="Path where the val images are. Default: $PWD."
         )
     parser.add_argument(
@@ -55,41 +65,80 @@ def main():
         )
 
     args = parser.parse_args()
-
-    train_generator = dp.train_data_generator(
-        args.path, args.width, args.height)
-    validation_generator = dp.val_data_generator(
-        args.valpath, args.width, args.height)
-
-    nb_train_samples = train_generator.nb_sample
-    nb_val_samples = validation_generator.nb_sample
-    print("Number of training samples: %i " % nb_train_samples)
-    print("Number of training samples: %i " % nb_val_samples)
-    nb_class = train_generator.nb_class
-    print('Total classes are %i' % nb_class)
+    sgd = SGD(lr=args.lr, decay=0.0002, momentum=0.9)
 
     t0 = time.time()
-    print "Building the model"
-    model = km.SqueezeNet(
-        nb_class, inputs=(args.channels, args.height, args.width))
-    dp.visualize_model(model)
+    if args.action == 'train':
+        train_generator = dp.train_data_generator(
+            args.path, args.width, args.height)
+        validation_generator = dp.val_data_generator(
+            args.valpath, args.width, args.height)
 
-    sgd = SGD(lr=args.lr, decay=0.0002, momentum=0.9)
-    model.compile(
-        optimizer=sgd, loss='categorical_crossentropy', metrics=['accuracy'])
-    print "Model built"
+        classes = train_generator.class_indices
+        nb_train_samples = train_generator.nb_sample
+        nb_val_samples = validation_generator.nb_sample
+        print("[squeezenet_demo] N training samples: %i " % nb_train_samples)
+        print("[squeezenet_demo] N validation samples: %i " % nb_val_samples)
+        nb_class = train_generator.nb_class
+        print('[squeezenet_demo] Total classes are %i' % nb_class)
 
-    print "Training"
-    model.fit_generator(
-        train_generator,
-        samples_per_epoch=nb_train_samples,
-        nb_epoch=args.epochs,
-        validation_data=validation_generator,
-        nb_val_samples=nb_val_samples)
+        t0 = tl.print_time(t0, 'initialize data')
+        model = km.SqueezeNet(
+            nb_class, inputs=(args.channels, args.height, args.width))
+        dp.visualize_model(model)
+        t0 = tl.print_time(t0, 'build the model')
 
-    print "Model trained"
+        model.compile(
+            optimizer=sgd, loss='categorical_crossentropy',
+            metrics=['accuracy'])
+        t0 = tl.print_time(t0, 'compile model')
 
-    model.save_weights('./weights.h5', overwrite=True)
+        model.fit_generator(
+            train_generator,
+            samples_per_epoch=nb_train_samples,
+            nb_epoch=args.epochs,
+            validation_data=validation_generator,
+            nb_val_samples=nb_val_samples)
+        t0 = tl.print_time(t0, 'train model')
+
+        model.save_weights('./weights.h5', overwrite=True)
+        model_parms = {'nb_class': nb_class,
+                       'nb_train_samples': nb_train_samples,
+                       'nb_val_samples': nb_val_samples,
+                       'classes': classes,
+                       'channels': args.channels,
+                       'height': args.height,
+                       'width': args.width}
+        tl.write_json(model_parms, fname='./model_parms.json')
+        t0 = tl.print_time(t0, 'save model')
+
+    elif args.action == 'predict':
+        _parms = tl.parse_json('./model_parms.json')
+        model = km.SqueezeNet(
+            _parms['nb_class'],
+            inputs=(_parms['channels'], _parms['height'], _parms['width']),
+            weights_path='./weights.h5')
+        dp.visualize_model(model)
+        model.compile(
+            optimizer=sgd, loss='categorical_crossentropy',
+            metrics=['accuracy'])
+
+        X_test, Y_test, classes, F = dp.prepare_data_test(
+            args.path, args.width, args.height)
+        t0 = tl.print_time(t0, 'prepare data')
+
+        outputs = []
+        results = model.predict(
+            X_test, batch_size=args.batchsize, verbose=1)
+        classes = _parms['classes']
+        for i in range(0, len(F)):
+            _cls = results[i].argmax()
+            max_prob = results[i][_cls]
+            outputs.append({'input': F[i], 'max_probability': max_prob})
+            cls = [key for key in classes if classes[key] == _cls][0]
+            outputs[-1]['class'] = cls
+            print('[squeezenet_demo] %s: %s (%.2f)' % (F[i], cls, max_prob))
+        t0 = tl.print_time(t0, 'predict')
 
 if __name__ == '__main__':
     main()
